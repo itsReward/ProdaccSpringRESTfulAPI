64d49b89edf4ecf3abea7f2aba3f8504f76f51b9
ItsReward <rewardmoyo2108@gmail.com>
2024-06-21 15:54:56 +0200

    first commit

---
+++ src/test/kotlin/org/prodacc/webapi/WebApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.prodacc.webapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
