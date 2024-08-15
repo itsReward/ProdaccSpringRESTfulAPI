b51bfdabd2d255e9c340bb162a11b7c368b84cab
ItsReward <rewardmoyo2108@gmail.com>
2024-08-15 11:23:35 +0200

    aws intergration

--- src/main/kotlin/org/prodacc/webapi/LambdaHandler.kt
+++ src/main/kotlin/org/prodacc/webapi/LambdaHandler.kt
@@ -1,6 +1,5 @@
 package org.prodacc.webapi
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse
 import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler
@@ -11,13 +10,21 @@ import javax.ws.rs.core.Application
 class LambdaHandler : RequestHandler<AwsProxyRequest, AwsProxyResponse> {
     private val handler: SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>
 
-    init {
+    /*init {
         try {
             handler = SpringLambdaContainerHandler.getAwsProxyHandler(Application::class.java)
         } catch (e: ContainerInitializationException) {
             throw RuntimeException("Could not initialize Spring Boot application", e)
         }
+    }*/
+
+    init {
+        val classLoader = Thread.currentThread().contextClassLoader
+        val clazz = classLoader.loadClass("com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler")
+        val method = clazz.getMethod("getAwsProxyHandler", Class::class.java)
+        handler = method.invoke(null, Application::class.java) as SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>
     }
+
 
     override fun handleRequest(input: AwsProxyRequest, context: Context): AwsProxyResponse {
         return handler.proxy(input, context)
