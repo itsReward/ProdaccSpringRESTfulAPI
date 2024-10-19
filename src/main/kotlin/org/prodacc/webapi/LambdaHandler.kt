2ea94e83a22d0cf049eb453da3e34e97834b7aac
ItsReward <rewardmoyo2108@gmail.com>
2024-10-19 17:04:11 +0200

    Update LambdaHandler.kt

--- src/main/kotlin/org/prodacc/webapi/LambdaHandler.kt
+++ src/main/kotlin/org/prodacc/webapi/LambdaHandler.kt
@@ -1,32 +1,27 @@
 package org.prodacc.webapi
 
+import com.amazonaws.serverless.exceptions.ContainerInitializationException
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse
 import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler
 import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler
+import java.io.InputStream
+import java.io.OutputStream
 import javax.ws.rs.core.Application
 
-class LambdaHandler : RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+class LambdaHandler : RequestStreamHandler {
     private val handler: SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>
 
-    /*init {
+    init {
         try {
             handler = SpringLambdaContainerHandler.getAwsProxyHandler(Application::class.java)
         } catch (e: ContainerInitializationException) {
             throw RuntimeException("Could not initialize Spring Boot application", e)
         }
-    }*/
-
-    init {
-        val classLoader = Thread.currentThread().contextClassLoader
-        val clazz = classLoader.loadClass("com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler")
-        val method = clazz.getMethod("getAwsProxyHandler", Class::class.java)
-        handler = method.invoke(null, Application::class.java) as SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>
     }
 
-
-    override fun handleRequest(input: AwsProxyRequest, context: Context): AwsProxyResponse {
-        return handler.proxy(input, context)
+    override fun handleRequest(inputStream: InputStream, outputStream: OutputStream, context: Context) {
+        handler.proxyStream(inputStream, outputStream, context)
     }
 }
